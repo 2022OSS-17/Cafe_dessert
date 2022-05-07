658c3bdab31b360cec7c620e22c9c283b02a7048
Blackcode2 <doky03115@gmail.com>
2022-05-08 01:34:49 +0900

    stdio.h 추가, 함수 설명 주석 추가, 개행문자 없애는 코드 추가

--- product.c
+++ product.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 #include "product.h"
 
+// 디저트 생성하는 함수
 int createProduct(Product *p){
     printf("\n");
     printf("제품명은? ");
     scanf("%[^\n]s",p->name);
+    getchar();
 
     printf("중량은? ");
     scanf("%d",&p->weight);
@@ -15,15 +18,18 @@ int createProduct(Product *p){
     return 1;
 }
 
+// 하나의 디저트 품목 출력 함수
 void readProduct(Product *p){
     printf("%-15s %3dg %4d원\n",p->name,p->weight,p->price);
 }
 
+// 생성된 디저트 수정 함수
 int updateProduct(Product *p){
 
     printf("\n");
     printf("제품명은? ");
     scanf("%[^\n]s",p->name);
+    getchar();
 
     printf("중량은? ");
     scanf("%d",&p->weight);
@@ -34,6 +40,7 @@ int updateProduct(Product *p){
     return 1;
 };
 
+// 생성된 디저트 품목 삭제 함수
 int deleteProduct(Product *p){
     p->weight=-1;
     p->price=-1;
