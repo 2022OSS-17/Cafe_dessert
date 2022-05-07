b5c3f7f06431c436c9617b7d224cf179abc27fbe
Blackcode2 <doky03115@gmail.com>
2022-05-08 00:56:52 +0900

    함수 설명하는 주석 추가하고 문법 오류 수정.

--- manager.h
+++ manager.h
@@ -1,8 +1,16 @@
 #include "product.h"
 
+// 자판기 메뉴 보여주는 함수
 int selectMenu();
+
+// 저장된 디저트 목록 보여주는 함수
 void listProduct(Product *p,int count);
+
+// 번호를 통해서 디저트 선택하는 함수
 int selectDataNo(Product *p, int count);
 
+// 디저트 리스트 저장하는 함수
 void saveData(Product *p, int count);
-int loadData(Product *p)
+
+// 저장된 리스트 불러오는 함수
+int loadData(Product *p);
